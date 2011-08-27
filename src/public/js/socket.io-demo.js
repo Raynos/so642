5d15cc53a30b2f5bbe83ac9845fc16beb8a2cfed
Raynos <raynos2@gmail.com>
2011-08-27 02:29:52 +0100

    Made an actual chat

--- src/public/js/socket.io-demo.js
+++ src/public/js/socket.io-demo.js
@@ -1,5 +1,28 @@
-(function() {
+window.onload = function() {
 	var messages = io.connect('/messages');
 
-	
-})();
+	messages.on("newMessage", function(data) {
+		var span = document.createElement("div");
+
+		span.textContent = data.userId + " : " + data.text
+		main.insertBefore(span, input);
+	});
+
+
+
+	var main = document.getElementById("main");
+
+	var msg = document.createElement("button");
+	msg.textContent = "Send message";
+	msg.addEventListener("click", function() {
+		messages.emit("sendMessage", {
+			text: input.value
+		})
+	}, false);
+
+	var input = document.createElement("input");
+
+	main.appendChild(input);
+	main.appendChild(msg);
+
+};
--- src/route/messages.js
+++ src/route/messages.js
@@ -3,7 +3,13 @@ module.exports = function _route(app, model, io) {
 	var messages = io.of('/messages');
 		
 	messages.on("connection", function (socket) {
-		console.log(socket);
-	});
 
+		socket.on("sendMessage", function(data) {
+			
+			messages.emit("newMessage", {
+				userId: socket.id,
+				text: data.text
+			});
+		});
+	});
 };
