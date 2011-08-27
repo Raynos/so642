5413dee5fa61d678e892552659952491662254b5
yojimbo87 <bosak.tomas@gmail.com>
2011-08-27 23:03:13 +0200

    Added user model methods for managing users rooms in which they are currently in and room model method for removing current user from room

--- src/model/rooms.js
+++ src/model/rooms.js
@@ -178,6 +178,30 @@ RoomModel.prototype.setCurrentUser = function(userID, roomID, callback) {
 };
 
 /*------------------------------------------------------------------------------
+  (public) unsetCurrentUser
+
+  + userID
+  + roomsID
+  + callback - err or native response
+  - void
+  
+  Removes user from specific room current users.
+------------------------------------------------------------------------------*/
+RoomModel.prototype.unsetCurrentUser = function(userID, roomID, callback) {
+	this._redisClient.srem(
+		"room:" + roomID + ":current_users", 
+		"user:" + userID,
+		function(err, res) {
+			if(err) {
+				callback(err, undefined);
+			} else {
+				callback(undefined, res);
+			}
+		}
+	);
+};
+
+/*------------------------------------------------------------------------------
   (public) getReadAccessUsers
 
   + roomID
