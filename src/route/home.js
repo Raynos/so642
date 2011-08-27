b9b9f9ce0c91001a4b7bdd2fd7d4c41dce0a3acb
Raynos <raynos2@gmail.com>
2011-08-27 02:00:27 +0100

    Added faq route

--- src/route/home.js
+++ src/route/home.js
@@ -1,5 +1,10 @@
 module.exports = function _route(app, model, io) {
+
 	app.get("/", function(req, res) {
 		res.render("home");
 	});
+
+	app.get("/faq", function(req, res) {
+		res.render("faq");
+	})
 };
